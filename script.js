0502cba7bd9d206a412c03998c13cde79df220d4
GuiMayer <Guigmayer@gmail.com>
2024-10-01 22:35:12 -0300

    Version 1.0.0
    Site Prototipado

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('scrollButton').addEventListener('click', function() {
-    document.querySelector('section:nth-of-type(2)').scrollIntoView({
-        behavior: 'smooth'
-    });
-});
